dcc2f54ab76aeffb171cdd8c99344117142c0be8
solariscoin <solariscoin@mail.com>
2017-03-18 19:55:49 -0400

    First commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define solariscoin-Qt message queue name
+#define solariscoinURI_QUEUE_NAME "solariscoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
